3bc7676d3139f1b62391d26187a5b57274cf2c97
Nikolay Lyubavin <nik.lubavin@gmail.com>
2020-01-29 21:50:52 +0300

    first commit

---
+++ 1.1-variables/main.js
@@ -0,0 +1,21 @@
+// ========== task 1 ===============
+let algebra = 5;
+let geography = 4;
+let physics = 5;
+
+let average = (algebra + geography + physics)/3;
+console.log(`Среднее по оценкам : ${average}`);
+
+// ========== task 2 ===============
+
+let myName = "Nikola";
+let message = `Привет мир! Меня зовут ${myName}`;
+
+console.log(message);
+// ========== task 3 ===============
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1 ;
+console.log(result);
