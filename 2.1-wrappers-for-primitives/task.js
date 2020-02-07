c6fe502cd5731f145324f381dc38146ef4eeab10
Nikolay Lyubavin <nik.lubavin@gmail.com>
2020-02-07 21:30:48 +0300

    2.1 homework

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -11,8 +13,39 @@ function calculateMortgage() {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+  if (isNaN(parseInt(percent))) {
+    return 'Incorrect "percent"';
+  }
+
+  if (isNaN(parseInt(contribution))) {
+    return 'Incorrect "contribution"';
+  }
+
+  if (isNaN(parseInt(amount))) {
+    return 'Incorrect "amount"';
+  }
+
+  if (isNaN(Date.parse(date)) ) {
+    return 'Incorrect "Date"';
+  }
+
+  let correctDate = new Date(date);
+  if (correctDate - new Date() < 0) {
+    return 'Incorrect "Date" (cannot be before Now)';
+  }
+
+  let sumToPay = amount - contribution;
+  if (sumToPay === 0) {
+    return '0';
+  }
+
+  let monthsToPay = Math.ceil((correctDate - new Date() )/(1000*3600*24*31) );
+  let p = percent/(100*12);
+
+  let payment = sumToPay*(p + p/( Math.pow( (1+p), monthsToPay) - 1 ) )
+  let totalAmount = payment*monthsToPay;
+
+  return totalAmount.toFixed(2);
 }
 
 function sayHello() {
@@ -23,6 +56,12 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+  let name_ = name;
+  if (!name_) {
+    name_ = 'Аноним';
+  }
+
+  let greeting = `Привет мир! Меня зовут ${name_}`;
+  console.log(greeting);
+  return greeting;
 }
